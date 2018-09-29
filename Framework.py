bc59969feb9675b57c052b11c485149a135074a3
Basant Singh <Basantmhr@users.noreply.github.com>
2018-09-29 12:35:44 +0530

    Basic Window
    basic window only

---
+++ Framework.py
@@ -0,0 +1,6 @@
+from tkinter import*
+root=Tk()
+root.title("Image Identification Captcha V.1 ")
+root.wm_iconbitmap('icc.ico') #Test case icon
+root.geometry('1920x766')
+root.mainloop()
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Image Identification Captcha
+This is Image Identification Captcha being Developed By Basant Singh in Python
+In future it can be connected with WebApplications 
