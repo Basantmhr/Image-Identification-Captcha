f7d8d498041f53511f52f8d7d599d70bb719186e
Basant Singh <Basantmhr@users.noreply.github.com>
2018-09-30 21:37:28 +0530

    added main page text and button
    text and button on main page

--- Framework.py
+++ Framework.py
@@ -1,6 +1,20 @@
 from tkinter import*
+def buttonclick(self):
+	print('You have click me.')
+
 root=Tk()
 root.title("Image Identification Captcha V.1 ")
 root.wm_iconbitmap('icc.ico') #Test case icon
 root.geometry('1920x766')
+c=Canvas(root,bg='grey',height=300,width='900',cursor='mouse')
+c.pack()
+fnt=('Times',40,'italic underline')
+id=c.create_text(500,100,text="Image Identification Captcha",font=fnt)
+f=Frame(root,height='500',width='1920',bg='blue',cursor='arrow')
+f.propagate(0)
+f.pack()
+b=Button(f,text='Run',width='5',height='2')
+b.pack()
+b.bind('<Button-1>',buttonclick)
+
 root.mainloop()
