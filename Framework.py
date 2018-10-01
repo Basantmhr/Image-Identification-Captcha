4735a4407dc7c134689b5d7fea2107edf7c6be29
Basant Singh <Basantmhr@users.noreply.github.com>
2018-10-01 10:32:32 +0530

    Quit button added
    added quit button on mainFrame

--- Framework.py
+++ Framework.py
@@ -1,7 +1,6 @@
 from tkinter import*
 def buttonclick(self):
 	print('You have click me.')
-
 root=Tk()
 root.title("Image Identification Captcha V.1 ")
 root.wm_iconbitmap('icc.ico') #Test case icon
@@ -13,8 +12,13 @@ id=c.create_text(500,100,text="Image Identification Captcha",font=fnt)
 f=Frame(root,height='500',width='1920',bg='blue',cursor='arrow')
 f.propagate(0)
 f.pack()
-b=Button(f,text='Run',width='5',height='2')
+b=Button(f,text='Start Image Identification Captcha',width='30',height='2',)
 b.pack()
+b.place(x=670,y=280)
 b.bind('<Button-1>',buttonclick)
+exit=Button(f,text='Exit',width='10',height='2',command=quit)
+exit.pack()
+exit.place(x=730,y=322)
+exit.bind('<Button-1>')
 
 root.mainloop()
